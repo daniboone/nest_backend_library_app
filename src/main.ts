a2fea3c95f0dbac91acdd0d53898688ba9c800ff
daniboone <daniboone123@gmail.com>
2021-10-13 13:27:47 -0500

    Swagger Initial Setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder, SwaggerCustomOptions } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const customOptions: SwaggerCustomOptions = {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+    customSiteTitle: 'My Library API Docs',
+  };
+
+  const config = new DocumentBuilder()
+    .setTitle('Library example')
+    .setDescription('Library Rest API description')
+    .setVersion('1.0')
+    .addTag('library')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document, customOptions);
+  
   await app.listen(3000);
 }
 bootstrap();
