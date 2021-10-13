023d47d36b64ea36be2dfa971edf9df029cad8a7
daniboone <daniboone123@gmail.com>
2021-10-13 12:57:59 -0500

    Enviroment Variables ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { getConnectionOptions } from 'typeorm';
 import { UsersModule } from './users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [TypeOrmModule.forRootAsync({
-    useFactory: async () =>
-      Object.assign(await getConnectionOptions(), {
-        autoLoadEntities: true,
-      }),
+    imports: [ConfigModule.forRoot({cache: true})],    
+    useFactory: (configService: ConfigService) => ({
+      type: 'postgres',
+      host: configService.get('HOST'),
+      port: +configService.get<number>('PORT'),
+      username: configService.get('USERNAME'),
+      password: configService.get('PASSWORD'),
+      database: configService.get('DATABASE'),
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,      
+    }),
+    inject: [ConfigService],
   }),
   UsersModule],
   controllers: [AppController],
